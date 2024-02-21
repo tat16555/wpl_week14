cbfd1106cdb61c75ab45d273e917ca1b971e50a8
tat16555 <pannatat.b@gmail.com>
2024-02-21 10:10:11 +0700

    first commit

--- users/create.php
+++ users/create.php
@@ -2,25 +2,30 @@
 session_start();
 if(isset($_SERVER["REQUEST_METHOD"])){
     extract($_POST);
-    $shapassword = password_hash($password, PASSWORD_DEFAULT);
-    $param = Array(
-        'u_name' => $u_name,
-        'u_email' => $u_email,
-        'password' => $shapassword,
-        'created_at' => date("Y-m-d h:i:s"),
-        'updated_at' => date("Y-m-d h:i:s"),
-        'Status' => $Status
-    );
-    require_once '../common/connect.php';
-    //เตรียมคำสั่ง SQL
-    $ststement = $conn->prepare("INSERT INTO users(u_name, u_email, password, created_at, updated_at, Status)
-                                            VALUES(:u_name, :u_email, :password, :created_at, :updated_at, :Status)");
-
-    if($ststement->execute($param)){
-        $_SESSION['success'] = "ลงทะเบียนสำเร็จ";
-        header("Location: index.php");
+    if($password === $c_password){
+        $shapassword = password_hash($password, PASSWORD_DEFAULT);
+        $param = Array(
+            'u_name' => $u_name,
+            'u_email' => $u_email,
+            'password' => $shapassword,
+            'created_at' => date("Y-m-d h:i:s"),
+            'updated_at' => date("Y-m-d h:i:s"),
+            'Status' => $Status
+        );
+        require_once '../common/connect.php';
+        //เตรียมคำสั่ง SQL
+        $ststement = $conn->prepare("INSERT INTO users(u_name, u_email, password, created_at, updated_at, Status)
+                                                VALUES(:u_name, :u_email, :password, :created_at, :updated_at, :Status)");
+    
+        if($ststement->execute($param)){
+            $_SESSION['success'] = "ลงทะเบียนสำเร็จ";
+            header("Location: index.php");
+        } else {
+            $_SESSION['error'] = "ลงทะเบียนไม่สำเร็จ";
+            header("Location: index.php");
+        } 
     } else {
-        $_SESSION['error'] = "ลงทะเบียนไม่สำเร็จ";
+        $_SESSION['error'] = "passwordไม่ถูกต้อง";
         header("Location: index.php");
     } 
 } else {
@@ -28,3 +33,4 @@ if(isset($_SERVER["REQUEST_METHOD"])){
     header("Location: index.php");
     exit();
 }
+?>
